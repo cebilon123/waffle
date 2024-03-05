cb9449817f07c9293fb53aca89b84836e9e362ef
Michał Kowal <michalkowal567@gmail.com>
2024-03-05 14:48:14 +0100

    Simplify main, add  server package

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,60 +1,25 @@
 package main
 
 import (
+	"context"
 	"embed"
 	_ "embed"
-	"log"
-
-	"waffle/internal/certificate"
-	"waffle/internal/config"
-	"waffle/internal/domain"
-	"waffle/internal/proxy"
+	"fmt"
+	"os"
+	
+	"waffle/cmd/proxy/server"
 )
 
 //go:embed config/config.yml
 var yamlConfigBytes []byte
 
 //go:embed .cert/*
-var Certificates embed.FS
+var certificates embed.FS
 
 func main() {
-	_, err := config.LoadEnvironmentConfig()
-	if err != nil {
-		log.Fatal(err.Error())
+	ctx := context.Background()
+	if err := server.Run(ctx, yamlConfigBytes, certificates); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
 	}
-
-	yamlCfg, err := config.NewYamlConfig(yamlConfigBytes)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	yamlDnsProvider := domain.NewYamlNameSystemProvider(yamlCfg)
-
-	certificateProvider := certificate.NewLocalCertificatesProvider(loadLocalCustomCACerts(), loadLocalCertPEMBlock(), loadLocalKeyPEMBlock())
-
-	server := proxy.NewServer(yamlDnsProvider, ":8080", certificateProvider)
-
-	log.Println("Starting Waffle Proxy on port :8080 🚀")
-
-	if err := server.Start(); err != nil {
-		log.Fatal(err.Error())
-	}
-}
-
-func loadLocalCustomCACerts() [][]byte {
-	certBytes, _ := Certificates.ReadFile(".cert/ca.crt")
-
-	return [][]byte{certBytes}
-}
-
-func loadLocalCertPEMBlock() []byte {
-	certBytes, _ := Certificates.ReadFile(".cert/server.crt")
-
-	return certBytes
-}
-
-func loadLocalKeyPEMBlock() []byte {
-	certBytes, _ := Certificates.ReadFile(".cert/server.key")
-
-	return certBytes
 }
---
+++ cmd/proxy/server/server.go
@@ -0,0 +1,65 @@
+package server
+
+import (
+	"context"
+	"embed"
+	"log"
+	"os"
+	"os/signal"
+
+	"waffle/internal/certificate"
+	"waffle/internal/config"
+	"waffle/internal/domain"
+	"waffle/internal/proxy"
+)
+
+func Run(ctx context.Context, yamlConfigBytes []byte, certificates embed.FS) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	_, err := config.LoadEnvironmentConfig()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	yamlCfg, err := config.NewYamlConfig(yamlConfigBytes)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	yamlDnsProvider := domain.NewYamlNameSystemProvider(yamlCfg)
+
+	certificateProvider := certificate.NewLocalCertificatesProvider(
+		loadLocalCustomCACerts(certificates),
+		loadLocalCertPEMBlock(certificates),
+		loadLocalKeyPEMBlock(certificates),
+	)
+
+	proxyServer := proxy.NewServer(yamlDnsProvider, ":8080", certificateProvider)
+
+	log.Println("Starting Waffle Proxy on port :8080 🚀")
+
+	if err := proxyServer.Start(); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	return nil
+}
+
+func loadLocalCustomCACerts(certificates embed.FS) [][]byte {
+	certBytes, _ := certificates.ReadFile(".cert/ca.crt")
+
+	return [][]byte{certBytes}
+}
+
+func loadLocalCertPEMBlock(certificates embed.FS) []byte {
+	certBytes, _ := certificates.ReadFile(".cert/server.crt")
+
+	return certBytes
+}
+
+func loadLocalKeyPEMBlock(certificates embed.FS) []byte {
+	certBytes, _ := certificates.ReadFile(".cert/server.key")
+
+	return certBytes
+}
