5c251a91c3d515a901c28f72a581f35e582ec82a
Michał Kowal <michalkowal567@gmail.com>
2024-03-13 16:06:41 +0100

    #7 Add useful comments for the wrapper

--- internal/request/wrapper.go
+++ internal/request/wrapper.go
@@ -5,8 +5,11 @@ import (
 	"net/http"
 )
 
+// Wrapper is used to wrap request, in order to not pass it every time.
 type Wrapper struct {
-	Request   *http.Request
+	Request *http.Request
+	// IPAddress is a real ip address of the client. The difference between this one and request remoteAddr is
+	// that it can be read from the headers (i.e. if request was forwarded by some kind of proxy).
 	IPAddress *net.IP
 }
 
