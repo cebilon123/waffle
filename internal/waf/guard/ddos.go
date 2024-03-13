0cae4397b718d1f4e971a1f7575689dbd70d0588
Michał Kowal <michalkowal567@gmail.com>
2024-03-13 15:26:47 +0100

    #7 Add outline of DDOS guard

--- internal/waf/guard/ddos.go
+++ internal/waf/guard/ddos.go
@@ -1,1 +1,12 @@
 package guard
+
+import "waffle/internal/ddos"
+
+type DDOS struct {
+	ipValidator ddos.IPValidator
+}
+
+func (D *DDOS) Validate(rw *RequestWrapper) error {
+	//TODO implement me
+	panic("implement me")
+}
