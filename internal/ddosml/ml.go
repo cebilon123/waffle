5f0bd5620663a50e7253d9e6a274ba74f5b6c89d
Michał Kowal <michalkowal567@gmail.com>
2024-05-04 07:34:43 +0200

    Add DDOS constructor

--- internal/ddosml/ml.go
+++ internal/ddosml/ml.go
@@ -5,3 +5,10 @@ package ddosml
 type DDOS struct {
 	isEnabled bool
 }
+
+// NewDDOS creates new ddos ML analyzer used to
+// analyze requests in order to find out if given
+// request is ddos attack or not.
+func NewDDOS(isEnabled bool) *DDOS {
+	return &DDOS{isEnabled: isEnabled}
+}
